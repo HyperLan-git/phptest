c8aa98df11043a511c614d78077a7e099bcb8d76
HyperLan <rekikyouness6@gmail.com>
2021-10-14 18:17:58 +0200

    First sql request while giving away a password to the entire internet !

--- hello.php
+++ hello.php
@@ -1,6 +1,10 @@
 <?php
-    print("This is a test lol\nI like tests\n");
-    echo 'CACA'
+    echo 'This hsould print users\n';
+    $conn = new PDO("mysql:host=localhost;dbname=","root","tesmoche");
+    $sql =  'SELECT * FROM \'user\'';
+    foreach  ($conn->query($sql) as $row) {
+        print $row['User'];
+    }
 ?>
 
 
