d21caae1035553c10c215374a328060cdeb86312
HyperLan <rekikyouness6@gmail.com>
2021-10-14 18:21:24 +0200

    yay security brech for a database I don't care about

--- hello.php
+++ hello.php
@@ -1,16 +1,9 @@
 <?php
-    echo 'This hsould print users\n';
-    $conn = new PDO("mysql:host=localhost;dbname=","root","tesmoche");
+    echo 'This hsould print users dzdzd
+        ';
+    $conn = new PDO("mysql:host=localhost;dbname=mysql","root","tesmoche");
     $sql =  'SELECT * FROM \'user\'';
     foreach  ($conn->query($sql) as $row) {
         print $row['User'];
     }
 ?>
-
-
-
-
-
-
-
-
