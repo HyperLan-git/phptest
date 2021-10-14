5ce5e57fd91fde2b73b0ff1de9a14a327a37c320
HyperLan <rekikyouness6@gmail.com>
2021-10-14 18:27:02 +0200

    Testing more

--- hello.php
+++ hello.php
@@ -2,7 +2,7 @@
     echo 'This hsould print users lul
         ';
     $conn = new PDO("mysql:host=localhost;dbname=mysql","root","tesmoche");
-    $sql =  "SELECT * FROM 'user'";
+    $sql =  "SELECT * FROM user";
     foreach  ($conn->query($sql) as $row) {
         print $row['User'] . "\n";
     }
