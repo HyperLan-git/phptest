790bd56aed1ebe022761f653135552b2b6bfde0b
HyperLan <rekikyouness6@gmail.com>
2021-10-14 18:27:49 +0200

    Testening

--- hello.php
+++ hello.php
@@ -4,6 +4,6 @@
     $conn = new PDO("mysql:host=localhost;dbname=mysql","root","tesmoche");
     $sql =  "SELECT * FROM user";
     foreach  ($conn->query($sql) as $row) {
-        print $row['User'] . "\n";
+        echo $row['User'] . "\n.";
     }
 ?>
