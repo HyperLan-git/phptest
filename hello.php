9ceb6d104d2c348921e9841c9114eeec12d9d0fb
HyperLan <rekikyouness6@gmail.com>
2021-10-14 18:24:43 +0200

    why is it not working

--- hello.php
+++ hello.php
@@ -2,7 +2,7 @@
     echo 'This hsould print users dzdzd
         ';
     $conn = new PDO("mysql:host=localhost;dbname=mysql","root","tesmoche");
-    $sql =  'SELECT * FROM \'user\'';
+    $sql =  'SELECT * FROM `user`';
     foreach  ($conn->query($sql) as $row) {
         print $row['User'];
     }
