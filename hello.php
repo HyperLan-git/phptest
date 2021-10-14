5b98bba83281d025cba859f0b874298a0757b4f7
HyperLan <rekikyouness6@gmail.com>
2021-10-14 18:26:14 +0200

    Testing stuffs

--- hello.php
+++ hello.php
@@ -1,9 +1,9 @@
 <?php
-    echo 'This hsould print users dzdzd
+    echo 'This hsould print users lul
         ';
     $conn = new PDO("mysql:host=localhost;dbname=mysql","root","tesmoche");
-    $sql =  'SELECT * FROM `user`';
+    $sql =  "SELECT * FROM 'user'";
     foreach  ($conn->query($sql) as $row) {
-        print $row['User'];
+        print $row['User'] . "\n";
     }
 ?>
