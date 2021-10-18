db53f5bdceda39499c1332ec3216a3d23751d988
HyperLan <rekikyouness6@gmail.com>
2021-10-18 16:13:10 +0200

    bruh

--- hello.php
+++ hello.php
@@ -1,9 +1,13 @@
 <?php
-    echo 'This hsould print users lul
-        ';
+    echo 'This hsould print users lul <br>';
     $conn = new PDO("mysql:host=localhost;dbname=mysql","root","tesmoche");
     $sql =  "SELECT * FROM user";
     foreach  ($conn->query($sql) as $row) {
-        echo $row['User'] . "<br>.";
+        echo "<br>" . $row['User'];
     }
+    echo '<br>
+        <form action="welcome_get.php" method="get">
+            Name: <input type="text" name="name"><br>
+            <input type="submit">
+        </form>'
 ?>
