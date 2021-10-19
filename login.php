319dd87519a51ee5cc79be8e7f0c55d5fa35223d
HyperLan <rekikyouness6@gmail.com>
2021-10-19 16:51:11 +0200

    Approaching target

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
     echo json_encode("POST : <br>name : " . $_POST['username'] .
-        "<br>hashtype : " . $_POST['passwordhash']);
+        "<br>hashtype : " . $_POST['hashtype'] .
+        "<br>hash : " . $_POST['passwordhash']);
 ?>
