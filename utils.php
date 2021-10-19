9ed89ca880dbd582df365b4649b4e39fa96c293d
HyperLan <rekikyouness6@gmail.com>
2021-10-19 23:48:07 +0200

    OwO

--- utils.php
+++ utils.php
@@ -1,11 +1,11 @@
 <?php
 define("SUPPORTED_HASHES", array("SHA-256", "SHA-512"));
-define("HEX_CHARS", "0123456789abcdef");
+define("HEX_CHARS", "0123456789abcdefABCDEF");
 
 function check_hash($type, $hash) {
 	switch($type) {
 		case 'SHA-256':
-			if(strlen($hash) != 64)
+			if(strlen($hash) != 65)
 				return 0;
 
 			foreach(str_split($hash) as $c)
