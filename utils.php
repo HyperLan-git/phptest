c7fba5fd1a2167dbc6b41487e1fb042737e8d643
HyperLan <rekikyouness6@gmail.com>
2021-10-19 19:27:28 +0200

    missed a syntax error

--- utils.php
+++ utils.php
@@ -1,15 +1,15 @@
 <?php
 define("SUPPORTED_HASHES", array("SHA-256", "SHA-512"));
+define("HEX_CHARS", "0123456789abcdef");
 
 function check_hash($type, $hash) {
-	const var hex_chars = "0123456789abcdef";
 	switch($type) {
 		case 'SHA-256':
 			if(strlen($hash) != 64)
 				return 0;
 
 			foreach(str_split($hash) as $c)
-				if(!str_contains(hex_chars, $c))
+				if(!str_contains(HEX_CHARS, $c))
 					return 0;
 
 			return 1;
